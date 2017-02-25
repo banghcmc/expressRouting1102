e05165438f778c1c889b5ba8596348413d787156
Admin <vanpho02@gmail.com>
2017-02-25 16:17:37 +0700

    ket thuc

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ let app = express();
 let PhepTinh = require('./PhepTinh.js');
 let parser = require('body-parser').urlencoded({extended: false});
 
-app.use('/s',express.static('public'));
+// app.use((req, res, next) => {
+//   console.log('Xin chao');
+//   req.name = 'KhoaPham'
+//   // res.send('hello')
+//   // next();
+// });
+
+app.use(express.static('public'));
+
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.listen(3000, () => console.log('Server started'));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello' + req.name));
 
 // app.get('/signup', (req, res) => {
 //   res.send(`
@@ -31,3 +39,16 @@ app.get('/', (req, res) => res.send('Hello'));
 app.get('/tinh', require('./controllers/xuLyTinh.js'));
 
 app.post('/xuly', parser, require('./controllers/xuly.js'));
+
+
+app.get('/show', (req, res, next) => {
+  req.name = 'aaa';
+  console.log('M1');
+  next();
+}, (req, res, next) => {
+  req.name = 'aaa';
+  console.log('M2');
+  next();
+}, (req, res) => {
+  res.send(req.name)
+});

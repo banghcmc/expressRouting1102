a9ae78d216f1acf1b7fb2d423eb0ee04f2e6b779
Admin <vanpho02@gmail.com>
2017-02-25 14:15:11 +0700

    post

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 let express = require('express');
 let app = express();
+
+let parser = require('body-parser').urlencoded({extended: false});
+
 app.listen(3000, () => console.log('Server started'));
 
 app.get('/', (req, res) => res.send('Hello'));
+
+app.get('/signup', (req, res) => {
+  res.send(`<form action="/dangky" method="post">
+    <input type="text" name="name" placeholder="Username">
+    <input type="password" name="password" placeholder="Password">
+    <input type="submit" value="Sign up">
+  </form>
+`);
+})
+
+app.post('/dangky', parser, (req, res) => {
+  console.log(req.body);
+  res.send('Dang ky thanh cong');
+});
